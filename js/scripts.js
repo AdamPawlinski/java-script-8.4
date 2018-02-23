76f0611d164f95879d07ccf038696fc638235ae7
Adam Pawliński <adam.pawlinski@gmail.com>
2018-02-23 07:26:30 +0100

    finish task

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+//just a begining
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+//alert('Hello, ' + name);
 console.log('Hello, ' + name);
+/*just few lines of comments:
+bla bla bla
+bla bla bla
+bla bla bla*/
